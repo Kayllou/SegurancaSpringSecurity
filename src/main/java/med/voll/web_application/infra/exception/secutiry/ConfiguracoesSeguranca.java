f0f43f9373dd6f05857afa5993a19e0d515a560c
Kayo Novais <kayonovais07@.gmail.com>
2026-07-11 16:15:53 -0300

    ajuste de permissões de visualização

--- src/main/java/med/voll/web_application/infra/exception/secutiry/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/exception/secutiry/ConfiguracoesSeguranca.java
@@ -2,11 +2,14 @@ package med.voll.web_application.infra.exception.secutiry;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,25 +18,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ConfiguracoesSeguranca {
 
     @Bean
-    public UserDetailsService dadosUsuariosCadastrados(){
-        UserDetails usuario1 = User.builder()
-                .username("joao@email.com")
-                .password("{noop}joao123")
-                .build();
-
-        UserDetails usuario2 = User.builder()
-                .username("maria@email.com")
-                .password("{noop}maria123")
-                .build();
-
-        return new InMemoryUserDetailsManager(usuario1, usuario2);
-    }
-
-    @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                    req.requestMatchers("/css/**", "/js/**", "/assets/**", "/", "/index", "/home").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .formLogin(form -> form.loginPage("/login")
@@ -43,9 +31,14 @@ public class ConfiguracoesSeguranca {
                         .logoutSuccessUrl("/login?logout")
                         .permitAll())
                 .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
-                        .alwaysRemember(true))
+                        .alwaysRemember(true)
+                )
+                .csrf(Customizer.withDefaults())
                 .build();
+    }
 
-        //token validaty seconds(111)
+    @Bean
+    public PasswordEncoder codificadorSenha(){
+        return new BCryptPasswordEncoder();
     }
 }
