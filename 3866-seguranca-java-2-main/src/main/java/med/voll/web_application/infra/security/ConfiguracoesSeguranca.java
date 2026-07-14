84a0a5dc341813780fa3f1b2d22c9bb6f8bb18fb
Kayo Novais <kayonovais07@.gmail.com>
2026-07-14 07:31:47 -0300

    Restricoes de acesso perfis front end

--- 3866-seguranca-java-2-main/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ 3866-seguranca-java-2-main/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -27,7 +27,7 @@ public class ConfiguracoesSeguranca {
                         req.requestMatchers(HttpMethod.GET, "/medicos").hasAnyRole("ATENDENTE", "PACIENTE");
                         req.requestMatchers("/medicos/**").hasRole("ATENDENTE");
                         req.requestMatchers(HttpMethod.POST, "/consultas/**").hasAnyRole("ATENDENTE", "PACIENTE");
-                        preq.requestMatchers(HttpMethod.PUT, "/consultas/**").hasAnyRole("ATENDENTE", "PACIENTE");
+                        req.requestMatchers(HttpMethod.PUT, "/consultas/**").hasAnyRole("ATENDENTE", "PACIENTE");
                         req.anyRequest().authenticated();
                     })
                 .formLogin(form -> form.loginPage("/login")
