92872528f9a374c516d987f9992aff29e20bfc59
Kayo Novais <kayonovais07@.gmail.com>
2026-07-14 22:56:11 -0300

    set token de expiracao

--- 3866-seguranca-java-2-main/src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
+++ 3866-seguranca-java-2-main/src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
@@ -54,5 +55,16 @@ public class UsuarioService implements UserDetailsService {
         logado.alterarSenha(senhaCriptografada);
 
         usuarioRepository.save(logado);
+    }
+
+    public void enviarToken(String email) {
+        Usuario usuario = usuarioRepository.findByEmailIgnoreCase(email).orElseThrow(
+                () -> new RegraDeNegocioException("usuario não encontrado")
+        );
+        String token = UUID.randomUUID().toString();
+        usuario.setToken(token);
+        usuario.setExpiracaoToken(LocalDateTime.now().plusMinutes(15));
+
+        usuarioRepository.save(usuario);
     }
 }
